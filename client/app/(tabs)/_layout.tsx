d8085f84fd304d6f445a71f48d548888585cc815
Varun-Karthik-T <varunkarthikceg@gmail.com>
2025-04-05 17:06:27 +0530

    [ui] stocks data done

--- client/app/(tabs)/_layout.tsx
+++ client/app/(tabs)/_layout.tsx
@@ -1,13 +1,12 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
-import Ionicons from 'react-native-vector-icons/Ionicons';
-import Foundation from 'react-native-vector-icons/Foundation'; 
-import MaterialIcons from '@expo/vector-icons/MaterialIcons';
-import { HapticTab } from '@/components/HapticTab';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
+import { Tabs } from "expo-router";
+import React from "react";
+import { Platform } from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import Foundation from "react-native-vector-icons/Foundation";
+import { HapticTab } from "@/components/HapticTab";
+import TabBarBackground from "@/components/ui/TabBarBackground";
+import { Colors } from "@/constants/Colors";
+import { useColorScheme } from "@/hooks/useColorScheme";
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -15,55 +14,66 @@ export default function TabLayout() {
   return (
     <Tabs
       screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'dark'].tint,
+        tabBarActiveTintColor: Colors[colorScheme ?? "dark"].tint,
         headerShown: false,
         tabBarButton: HapticTab,
         tabBarBackground: TabBarBackground,
         tabBarStyle: Platform.select({
           ios: {
-            position: 'absolute',
+            position: "absolute",
           },
           default: {},
         }),
-      }}>
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
-          title: 'Goal',
+          title: "Goal",
           tabBarIcon: ({ color }: { color: string }) => (
             <Foundation name="target" size={28} color={color} />
           ),
         }}
       />
-      <Tabs.Screen
-        name="settings"
-        options={{
-          title: 'Settings',
-          tabBarIcon: ({ color }: { color: string }) => (
-            <Ionicons name="settings" size={28} color={color} />
-          ),
-        }}
-      />
+
       <Tabs.Screen
         name="analyze"
         options={{
-          title: 'Analyze',
+          title: "Analyze",
           tabBarIcon: ({ color }: { color: string }) => (
-            <Ionicons name="analytics" size={28} color={color} />
+            <Ionicons name="bar-chart" size={28} color={color} />
           ),
         }}
       />
 
-    <Tabs.Screen
+      <Tabs.Screen
         name="payment"
         options={{
-          title: 'Payment',
+          title: "Payment",
           tabBarIcon: ({ color }: { color: string }) => (
             <Ionicons name="cash-outline" size={28} color={color} />
           ),
         }}
       />
+      <Tabs.Screen
+        name="invest"
+        options={{
+          title: "Invest",
+          tabBarIcon: ({ color }: { color: string }) => (
+            <Ionicons name="analytics-outline" size={28} color={color} />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: "Settings",
+          tabBarIcon: ({ color }: { color: string }) => (
+            <Ionicons name="settings" size={28} color={color} />
+          ),
+        }}
+      />
     </Tabs>
-    
   );
 }
